65d4ababbccb5a011332d027873caf3be7ccbe28
Chinmaya Krishnan Mahesh <cmahesh2@illinois.edu>
2018-11-29 16:30:56 -0600

    broken aes

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"crypto/rand"
-	"crypto/sha512"
 	"errors"
 	"github.com/andlabs/ui"
-	"golang.org/x/crypto/nacl/secretbox"
-	"golang.org/x/crypto/pbkdf2"
+	"io"
 	"io/ioutil"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
 )
 
 import _ "github.com/andlabs/ui/winmanifest"
@@ -26,64 +27,69 @@ func getfilename(window *ui.Window) string {
 
 // encrypt encrypts the given file. If successful, it returns nil else it returns an error.
 func encrypt(filename string, pass string) error {
-	var salt [16]byte
-	var nonce [24]byte
-	var secretKey [32]byte
-
-	_, err1 := rand.Read(salt[:]) // reads in a random salt.
-	check(err1)
-
-	_, err2 := rand.Read(nonce[:]) // reads in a random nonce.
-	check(err2)
-
 	f, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return errors.New("Error: Unable to read the file.")
 	}
+	key := []byte(pass)
 
-	secretbytes := pbkdf2.Key([]byte(pass), salt[:], 3*100000, 32, sha512.New) // generates the key from the password.
-	copy(secretKey[:], secretbytes[:])
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
 
-	encrypted := secretbox.Seal(nonce[:], []byte(f), &nonce, &secretKey)
-	// 3 is for determining the number of iters needed in the pbkdf2 function. multiply by 100,000 to get the iterations.
-	saltiters := append(salt[:], byte(3))
-	encrypted = append(saltiters, encrypted...)
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len([]byte(f)))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(f))
 
 	writef := filename + ".encrypted"
 
-	err3 := ioutil.WriteFile(writef, encrypted, 0644)
+	err3 := ioutil.WriteFile(writef, []byte(base64.URLEncoding.EncodeToString(ciphertext)) , 0644)
 	check(err3)
 	return nil
 }
 
 // decrypt decrypts the given file. If successful, it returns nil else it returns an error.
 func decrypt(filename string, pass string) error {
+	key := []byte(pass)
 
 	fb, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return errors.New("Error: Unable to read the file.")
 	}
 
-	var decryptNonce [24]byte
-	var salt [16]byte
-	var secretKey [32]byte
 
-	copy(salt[:], fb[:16]) // the salt is stored in the 1st 16 bytes of the file.
+	ciphertext, _ := base64.URLEncoding.DecodeString(string(fb[:]))
 
-	iters := int(fb[16]) // the number of iterations (* 100000) needed for the pbkdf2 function.
-
-	secretbytes := pbkdf2.Key([]byte(pass), salt[:], iters*100000, 32, sha512.New)
-	copy(secretKey[:], secretbytes[:])
-
-	copy(decryptNonce[:], fb[17:41]) //the nonce is stored after the slt and the number of iterations.
-	decrypted, ok := secretbox.Open([]byte{}, fb[41:], &decryptNonce, &secretKey)
-	if !ok {
-		return errors.New("Error: Unable to decrypt the file. Please check the password you entered and try again.")
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
 	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
+
 
 	writef := filename[:len(filename)-10] // removes the .encrypted extension.
 
-	err2 := ioutil.WriteFile(writef, decrypted, 0644)
+	err2 := ioutil.WriteFile(writef, []byte(ciphertext), 0644)
 	check(err2)
 	return nil
 }
